d91d938c43aa10cf989fdeadb9a2be621c02483c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-10-21 10:32:13 +1100

    added an edit button with popup window

--- maven/src/main/java/com/tasktracker/TaskManagerGUI.java
+++ maven/src/main/java/com/tasktracker/TaskManagerGUI.java
@@ -4,7 +4,10 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 public class TaskManagerGUI {
 
@@ -44,8 +47,17 @@ public class TaskManagerGUI {
             }
         });
 
+        // Create an Edit Button
+        Button editButton = new Button("Edit Selected");
+        editButton.setOnAction(e -> {
+            String selectedItem = taskListView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                openEditDialog(selectedItem, primaryStage);
+            }
+        });
+
         // Add UI elements to the VBox
-        root.getChildren().addAll(taskListView, taskInput, addButton, removeButton);
+        root.getChildren().addAll(taskListView, taskInput, addButton, removeButton, editButton);
 
         // Create the scene and set it to the stage
         Scene scene = new Scene(root, 300, 400);
@@ -53,4 +65,57 @@ public class TaskManagerGUI {
 
         primaryStage.show();
     }
+
+    private static void openEditDialog(String task, Window owner) {
+        // Create a new stage for the edit dialog
+        Stage editStage = new Stage();
+        editStage.initModality(Modality.WINDOW_MODAL);
+        editStage.initOwner(owner);
+        editStage.initStyle(StageStyle.UTILITY);
+        editStage.setTitle("Edit Task");
+    
+        // Create UI elements for editing
+        Label startDateLabel = new Label("Start Date:");
+        DatePicker startDatePicker = new DatePicker();
+    
+        Label startTimeLabel = new Label("Start Time:");
+        TextField startTimeTextField = new TextField();
+    
+        Label endDateLabel = new Label("End Date:");
+        DatePicker endDatePicker = new DatePicker();
+    
+        Label endTimeLabel = new Label("End Time:");
+        TextField endTimeTextField = new TextField();
+    
+        Label detailsLabel = new Label("Details:");
+        TextArea detailsTextArea = new TextArea();
+    
+        // Create a Save button to save the edited task
+        Button saveButton = new Button("Save");
+        saveButton.setOnAction(e -> {
+            // Handle saving the edited task here
+            // You can access the values from the fields and update the task
+            // For example, retrieve values using startDatePicker.getValue(), startTimeTextField.getText(), etc.
+            // Close the edit stage when done
+            editStage.close();
+        });
+    
+        // Create a layout for the edit dialog
+        GridPane editRoot = new GridPane();
+        editRoot.setHgap(10);
+        editRoot.setVgap(10);
+        editRoot.setPadding(new Insets(10));
+        editRoot.addRow(0, startDateLabel, startDatePicker, startTimeLabel, startTimeTextField);
+        editRoot.addRow(1, endDateLabel, endDatePicker, endTimeLabel, endTimeTextField);
+        editRoot.addRow(2, detailsLabel, detailsTextArea);
+        editRoot.add(saveButton, 1, 3);
+    
+        // Create the scene for the edit dialog
+        Scene editScene = new Scene(editRoot, 400, 300);
+        editStage.setScene(editScene);
+    
+        // Show the edit dialog
+        editStage.showAndWait();
+    }
+    
 }
